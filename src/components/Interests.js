6e018fa3783f8648895c77c76b231a56004fe92c
Levente Ibrányi <ibranyilevente@gmail.com>
2022-10-22 14:19:37 +0200

    Interest basic

--- src/components/Interests.js
+++ src/components/Interests.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Interests = () => {
-  return <h1>Interests</h1>;
+  return (
+    <div className="other-info interests">
+      <h2>Interests</h2>
+      <p>
+        IT lover. Horror geek. Open-mindedness. Team player. Humble. Honorable.
+      </p>
+    </div>
+  );
 };
 
 export default Interests;
