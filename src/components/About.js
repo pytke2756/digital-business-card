30c26a6a85783717b9ed19a00edb4c165fc2fb85
Levente Ibrányi <ibranyilevente@gmail.com>
2022-10-22 14:34:36 +0200

    About section

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 const About = () => {
-  return <h1>About</h1>;
+  return (
+    <div className="other-info about">
+      <h2>About</h2>
+      <p>
+        I am a junior fullstack developer at DPDgroup IT Solutions in Hungary.
+        Mostly programing in Java 11 but I really like Python. I am interested
+        in DevOps
+      </p>
+    </div>
+  );
 };
 
 export default About;
