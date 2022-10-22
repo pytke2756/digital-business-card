74244533f28e811e29fc3e646565294abe15ca32
Levente Ibrányi <ibranyilevente@gmail.com>
2022-10-22 14:01:22 +0200

    Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,30 @@
 import React from "react";
 
 const Footer = () => {
-  return <h1>Footer</h1>;
+  return (
+    <footer>
+      <div className="footer-icon">
+        <span className="footer-icon-color">
+          <i class="fa-brands fa-square-twitter fa-2xl"></i>
+        </span>
+      </div>
+      <div className="footer-icon">
+        <span className="footer-icon-color">
+          <i class="fa-brands fa-square-facebook fa-2xl"></i>
+        </span>
+      </div>
+      <div className="footer-icon">
+        <span className="footer-icon-color">
+          <i class="fa-brands fa-square-instagram fa-2xl"></i>
+        </span>
+      </div>
+      <div className="footer-icon">
+        <span className="footer-icon-color">
+          <i class="fa-brands fa-square-github fa-2xl"></i>
+        </span>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
