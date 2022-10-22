3f3f3bc5e065d36ac8c53fd7bc4f861fabbc43ae
Levente Ibrányi <ibranyilevente@gmail.com>
2022-10-22 14:57:25 +0200

    Info section

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,7 +1,23 @@
 import React from "react";
+import image from "../assets/photo.jpg";
 
 const Info = () => {
-  return <h1>Info</h1>;
+  return (
+    <div>
+      <img className="info-img" src={image}></img>
+      <div className="basic-info">
+        <h1>Levente Ibrányi</h1>
+        <h2>Fullstack Developer</h2>
+        <h4>github.com/pytke2756</h4>
+        <button className="email-btn">
+          <i class="fa-solid fa-envelope"></i>Email
+        </button>
+        <button className="linkedin-btn">
+          <i class="fa-brands fa-linkedin"></i>LinkedIn
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Info;
